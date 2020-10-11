ba839c5971876be2a6866972af778164cfa97a38
Jeff Stringer <71564882+JeffAStringer@users.noreply.github.com>
2020-10-11 18:08:47 -0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,6 @@
+const disappear = (Element) => {
+    //console.log("element was clicked and ID is:" + )
+    //console.log(Element.style)
+
+    document.getElementById(Element.id).style.display = "none"
+}
